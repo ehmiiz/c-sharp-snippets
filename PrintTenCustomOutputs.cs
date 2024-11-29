7fa4af07c1560451057280e81d7e9173141806a4
elarsson <emil@wirely.se>
2024-11-29 08:34:06 +0100

    new c# learning proj

---
+++ PrintTenCustomOutputs.cs
@@ -0,0 +1,47 @@
+﻿/*
+    By Emil Larsson
+    Simple program to demonstrate C#'s syntax and features
+
+    Demonstrates:
+    - Multi line comments
+    - Single line comments
+    - Ask user for input
+    - Assign variable types
+    - Avoid debugger exceptions
+    - Creating a method
+    - For loops
+    - Switch statement
+    - String interpolation (avoid concatination as always)
+*/
+using System.Diagnostics;
+
+// Demonstrates how to gather information from the user
+// and variable declaration, allowing a nullable value (?)
+Console.WriteLine("Enter something to print 10 times: ");
+string? printThis = Console.ReadLine();
+
+// Avoids debugger exceptions while working with CLIs
+if (!Debugger.IsAttached)
+{
+    Console.Clear();
+}
+
+// Method that uses a switch statement
+string GetCounterSuffix(int number)
+{
+    return number switch
+    {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th",
+    };
+}
+
+// For loop that finally outputs the result 
+for (int i = 1; i < 11; i++)
+{
+    string counterSuffix = GetCounterSuffix(i);
+    string loopMsg = $"{printThis}: the {i}{counterSuffix} time!";
+    Console.WriteLine(loopMsg);
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# C# Snippets by Ehmiiz
+
+This repository contains personal `.cs` files
+
+## The goal
+
+- Demonstrate C# concepts
+- Language features
+- Common design patterns
+- tbd
+
+might delete or make private later hehe
